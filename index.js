c009d81e924a1eafca1a4448b752a86e2a879e16
Unknown <19081997J@gmail.com>
2017-05-23 14:12:46 +0300

    Интерфейс для командной строки и добавление пира

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var WebTorrent = require('webtorrent');
+var argv = require('optimist').argv;
 var clear = require("cli-clear");
 
 var client = new WebTorrent();
-var torrentData = "http://new-rutor.org/parse/d.rutor.org/download/564170";
+var torrentData = argv.path;            //for links to torrent (path on computer, link to .torrent, magnet, infohash,..)
+var downloadFolder = argv.folder;       //where download file
+var peer = argv.peer;                   //optional, if need to add peers
 
-client.add(torrentData, { path: __dirname }, function (torrent) {
+
+client.add(torrentData, { path: downloadFolder }, function (torrent) {      //create torrent process
 
     var interval = setInterval(function () {
         clear();
@@ -12,11 +16,19 @@ client.add(torrentData, { path: __dirname }, function (torrent) {
             'Progress: ' + (torrent.progress * 100).toFixed(1) + '% | ' +
             'Download speed: ' + (torrent.downloadSpeed / (1024 * 1024)).toFixed(1) + ' Mb/s | ' +
             'Time remaining: ' + (torrent.timeRemaining / 60000).toFixed(1) + ' min')
-    }, 1000);
+    }, 5000);
 
-    torrent.on('done', function () {
+    torrent.on('ready', function () {                       //add peer
+        if (peer !== undefined) {
+            if (!torrent.addPeer(peer))
+                console.log('something go wrong');
+        }
+
+    });
+
+    torrent.on('done', function () {                         //call when download finish
         console.log('torrent download finished');
         clearInterval(interval);
-        require('child_process').exec('start "" ' + torrent.path + ' ');
+        require('child_process').exec('start "" ' + torrent.path + ' '); //open folder with downloaded path
     });
 })
